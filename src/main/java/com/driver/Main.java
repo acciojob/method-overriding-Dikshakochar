54cc4b28f3874c037b16eecd9e040b7d084eeb73
Diksha kochar <kochardiksha1@gmail.com>
2023-02-07 22:28:56 +0530

    new commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
+
 public class Main {
-  
+    public class A{
+      public String meth()
+      {
+          return "Invoking method from class A";
+      }
+    }
+    public class B extends A{
+        public String meth()
+        {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public void main(String[] args) {
+        A a=new A();
+        a.meth();
+        B b=new B();
+        b.meth();
+
+    }
 }
